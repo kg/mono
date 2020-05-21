896fca59aa9a170b06042779f6ee9f760f19fd28
Katelyn Gadd <kg@luminance.org>
2020-05-21 03:28:50 -0700

    Be explicit about packaging mode

--- sdks/wasm/icu/build-icu.sh
+++ sdks/wasm/icu/build-icu.sh
@@ -21,7 +21,7 @@ git reset --hard origin/master
 
 mkdir -p $ICU4C/build
 cd $ICU4C/build
-ICU_DATA_FILTER_FILE=$SCRIPTDIR/filters.json ../configure --disable-renaming --disable-samples
+ICU_DATA_FILTER_FILE=$SCRIPTDIR/filters.json ../configure --disable-renaming --disable-samples --with-data-packaging=library
 make
 
 # exit 1
@@ -33,7 +33,7 @@ mkdir -p $ICU4C/wasm-usr
 source $TOPDIR/sdks/builds/toolchains/emsdk/emsdk_env.sh
 
 # --disable-shared: If we try to build shared libraries the wasm toolchain will choke on things like soname
-ICU_DATA_FILTER_FILE=$SCRIPTDIR/filters.json emconfigure ../configure --prefix=$ICU4C/wasm-usr --enable-static --disable-shared CXXFLAGS=-Wno-sign-compare --with-cross-build=$ICU4C/build --with-data-packaging=archive --disable-extras --disable-renaming --disable-samples
+ICU_DATA_FILTER_FILE=$SCRIPTDIR/filters.json emconfigure ../configure --prefix=$ICU4C/wasm-usr --enable-static --disable-shared CXXFLAGS=-Wno-sign-compare --with-cross-build=$ICU4C/build --disable-extras --disable-renaming --disable-samples --with-data-packaging=archive
 
 # even though we set --with-cross-build and we used emconfigure, autoconf may not have decided we're cross-compiling, so forcibly set that
 sed -i -e 's/cross_compiling = .*/cross_compiling = yes/g' icudefs.mk
